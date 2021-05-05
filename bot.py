e99d6977ca0bbe1c0bf65ca1fce0235992ea9034
Siam islam <77488919+Siam-Islam@users.noreply.github.com>
2021-05-05 15:23:31 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -99,7 +99,7 @@ async def start(bot, cmd):
                 disable_web_page_preview=True, quote=True)
                 
         except Exception as err:
-            await cmd.reply_text(f"**✔️File Generated Successfully.✔️**\n__")
+            await cmd.reply_text(f"**File Generated Successfully.✅**\n__")
 
 
 @Bot.on_message((filters.document | filters.video | filters.audio) & ~filters.edited)
