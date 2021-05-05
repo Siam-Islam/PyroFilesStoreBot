a55ac80d38d03a80c9ccbde0cf8aeb1e36495aa0
Siam islam <77488919+Siam-Islam@users.noreply.github.com>
2021-05-05 14:31:39 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -95,7 +95,7 @@ async def start(bot, cmd):
                 send_stored_file = await bot.forward_messages(chat_id=cmd.from_user.id, from_chat_id=Config.DB_CHANNEL,
                                                               message_ids=file_id)
             await send_stored_file.reply_text(
-                f"**✅ Video Generated Successfully ✅**\n\n__",
+                f"**✅ Generated Successfully ✅**\n\n__",
                 disable_web_page_preview=True, quote=True)
                 
         except Exception as err:
