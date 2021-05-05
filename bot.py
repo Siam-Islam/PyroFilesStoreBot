6086ae989fd0bd338d9e36a9866a3f7920e8e16a
Siam islam <77488919+Siam-Islam@users.noreply.github.com>
2021-05-05 13:53:36 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -94,8 +94,10 @@ async def start(bot, cmd):
             elif Config.FORWARD_AS_COPY is False:
                 send_stored_file = await bot.forward_messages(chat_id=cmd.from_user.id, from_chat_id=Config.DB_CHANNEL,
                                                               message_ids=file_id)
-            
+            await send_stored_file.reply_text(
+                Test",
                 disable_web_page_preview=True, quote=True)
+                
         except Exception as err:
             await cmd.reply_text(f"Something went wrong!\n\n**Error:** `{err}`")
 
