82d34f173d74038e1160abff6dee53261a49e26a
Siam islam <77488919+Siam-Islam@users.noreply.github.com>
2021-05-05 13:39:28 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -166,10 +166,8 @@ async def main(bot, message):
         try:
             forwarded_msg = await message.forward(Config.DB_CHANNEL)
             file_er_id = forwarded_msg.message_id
-            share_link = f"https://t.me/{Config.BOT_USERNAME}?start=A4F_{file_er_id}"
-            CH_edit = await bot.edit_message_reply_markup(message.chat.id, message.message_id,
-                                                          reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(
-                                                              "Get Sharable Stored Link", url=share_link)]]))
+            
+
             if message.chat.username:
                 await forwarded_msg.reply_text(
                     f"#CHANNEL_BUTTON:\n\n[{message.chat.title}](https://t.me/{message.chat.username}/{CH_edit.message_id}) Channel's Broadcasted File's Button Added!")
